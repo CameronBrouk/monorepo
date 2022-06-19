dd72ebb4bbe92a8d739b750c8972f9aa0b8a4722
Cameron Brouk <cameron@razer.localdomain>
2022-06-19 13:23:53 -0500

    Begin testing frontend / utils setup

--- stlswing/frontend/src/App.tsx
+++ stlswing/frontend/src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import { add } from '@stlswing/utils'
-import { Hello } from '@stlswing/components'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='max-w-full space-x-4 w-screen h-screen max-h-screen bg-gray-200 text-red-800 flex justify-center items-center text-xl'>
-      <div className='p-4 border bg-blue-200'>
-        <Hello a='fat' />
-      </div>
+      <div className='p-4 border bg-blue-200'></div>
       <header className='bg-pink-200 p-4'>
         <span className='flex space-x-4'>
           <button onClick={() => setCount((count) => count + 1)}>count</button>
