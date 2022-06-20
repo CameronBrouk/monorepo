7a9f74d4cfa184ba93c58044c9c7204cb5428ffc
Cameron Brouk <cameronbrouk@gmail.com>
2022-06-19 20:47:11 -0500

    Remove unneeded file

--- stlswing/utils/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.subtract = exports.add = exports.logHello = void 0;
-var logHello = function () { return console.log('hello'); };
-exports.logHello = logHello;
-var add = function (n, n2) { return n + n2; };
-exports.add = add;
-var subtract = function (n, n2) { return n - n2; };
-exports.subtract = subtract;
