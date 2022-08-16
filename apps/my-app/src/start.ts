1758fdfbce9cf770f3ad34217de4cfec6d6cf902
Cameron Brouk <cameronbrouk@gmail.com>
2022-08-16 15:31:39 -0500

    Remove useless apps

--- apps/my-app/src/start.ts
+++
@@ -1,9 +0,0 @@
-import { MyClass } from 'my-controls'
-import { Milliseconds } from '@monorepo-practice/interfaces'
-
-const a: Milliseconds = 7
-
-console.log(a + 7)
-
-let myClass: MyClass = new MyClass()
-myClass.doSomething()
