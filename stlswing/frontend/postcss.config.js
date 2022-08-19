180d4351e63c00916142e2b5d10052f1b3703eee
Cameron Brouk <cameronbrouk@gmail.com>
2022-08-18 20:05:24 -0500

    Remove deprecated frontend library

--- stlswing/frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer')
-    // I removed the thing below because it was causing errors
-    // ...(process.env.NODE_ENV === 'production' ? require('cssnano') : [])
-  ]
-}
