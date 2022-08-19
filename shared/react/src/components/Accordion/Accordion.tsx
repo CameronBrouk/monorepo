80c403a48e02b6f4f4ca7acc275ef205e6bb4692
Cameron Brouk <cameronbrouk@gmail.com>
2022-08-18 20:32:33 -0500

    Enhance Accordion component
    The panel content now waits for the panel to be open before displaying

--- shared/react/src/components/Accordion/Accordion.tsx
+++ shared/react/src/components/Accordion/Accordion.tsx
@@ -9,9 +9,13 @@ export type AccordionProps = {
   title: string
   children?: React.ReactNode
   className?: string
+  containerClasses?: string
+  buttonClasses?: string
+  panelClasses?: string
+  titleClasses?: string
 }
 
-export const Accordion = ({ children, title, className }: AccordionProps) => {
+export const Accordion = ({ children, title, ...props }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   const [panelRef, { height: panelContentHeight }] = useMeasure()
@@ -22,13 +26,17 @@ export const Accordion = ({ children, title, className }: AccordionProps) => {
   const svgClasses = isOpen ? 'rotate-180' : 'rotate-0'
 
   return (
-    <dl className='mx-auto border border-gray-300 shadow-sm rounded-md'>
+    <dl
+      className={`mx-auto border border-gray-300 shadow-sm rounded-md ${props.containerClasses}`}
+    >
       <dt className={'leading-7'}>
         <button
           onClick={() => setIsOpen((o) => !o)}
-          className='focus:outline-none focus:text-gray-900 hover:bg-gray-100 flex items-start justify-between w-full p-2 text-left text-gray-400 transition-all duration-500 cursor-pointer'
+          className={`focus:outline-none focus:text-gray-900 hover:bg-gray-100 flex items-center justify-between w-full p-2 text-left text-gray-400 transition-all duration-500 cursor-pointer ${props.buttonClasses}`}
         >
-          <h2 className='font-medium text-gray-700'>{title}</h2>
+          <h2 className={`font-medium text-gray-700 ${props.titleClasses}`}>
+            {title}
+          </h2>
           <span className='h-7 flex items-center ml-6'>
             <div>
               <FontAwesomeIcon
@@ -39,9 +47,9 @@ export const Accordion = ({ children, title, className }: AccordionProps) => {
           </span>
         </button>
       </dt>
-      <animated.div style={animateContentHeight}>
+      <animated.div style={animateContentHeight} className='overflow-hidden'>
         {isOpen && (
-          <div ref={panelRef} className={className}>
+          <div ref={panelRef} className={props.panelClasses}>
             {children}
           </div>
         )}
@@ -49,3 +57,5 @@ export const Accordion = ({ children, title, className }: AccordionProps) => {
     </dl>
   )
 }
+
+export default Accordion
